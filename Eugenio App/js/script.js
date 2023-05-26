2114fc2fd59f0c0337d89fde4ed2d1edf230bff7
Edgar Bruno Benicio <edgarbrunobenicio@gmail.com>
2023-05-26 13:26:54 -0300

    adicionando mudanças

--- Eugenio App/js/script.js
+++ Eugenio App/js/script.js
@@ -1,3 +1,7 @@
-function redirectToTBeam() {
-  window.location.href = "tBeam.html";
+function redirectToTBeam(x) {
+  if (x==1) {
+    window.location.href = "tBeam.html";  
+  } else {
+    window.location.href = "iBeam.html";
+  } 
 }
