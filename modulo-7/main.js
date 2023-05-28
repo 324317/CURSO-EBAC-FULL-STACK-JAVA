2c77c1268bbe10cda336d86a184db0a37de1d791
Edgar Bruno Benicio <edgarbrunobenicio@gmail.com>
2023-05-28 18:40:39 -0300

    atualizando

--- modulo-7/main.js
+++ modulo-7/main.js
@@ -1,6 +1,7 @@
 const form = document.getElementById('form');
 const campoA = document.getElementById('campo-A');
 const campoB = document.getElementById('campo-B');
+const confirmMessage = document.getElementById('confirm');
 let formEValido = false;
 
 function validaForm(campoA,campoB) {
@@ -15,38 +16,28 @@ form.addEventListener('submit', function(event) {
 
     const campoA = document.getElementById('campo-A');
     const campoB = document.getElementById('campo-B');
-    const sucessMessage = `<b>Campo B: ${campoB.value}</b> é maior que <b>campo A: ${campoA.value}</b>`;
-    const errorMessage = `<b>Campo B: ${campoB.value}</b> é menor (ou igual) a <b>Campo A: ${campoA.value}</b>`;
+    const sucessMessage = `Formulário Válido : <b>Campo B( ${campoB.value} )</b> é maior que <b>campo A ( ${campoA.value} )</b>`;
+    const errorMessage = `Formulário Inválido : <b>Campo B( ${campoB.value} )</b> é menor (ou igual) que <b>campo A ( ${campoA.value} )</b>`;
 
     formEValido = validaForm(campoA.value,campoB.value)
     if (formEValido) {
+        confirmMessage.classList.add('sucess-message')
         const containerMessageSucess = document.querySelector('.sucess-message');
         containerMessageSucess.innerHTML = sucessMessage;
-        containerMessageSucess.style.display='block';
 
         campoA.value='';
         campoB.value='';
 
     } else {
-        const containerMessageSucess = document.querySelector('.sucess-message');
-        containerMessageSucess.innerHTML = errorMessage;
-        containerMessageSucess.style.display='block';
+        confirmMessage.classList.add('error-message')
+        const containerMessageError = document.querySelector('.error-message');
+        containerMessageError.innerHTML = errorMessage;
 
         campoA.value='';
         campoB.value='';
     }
 })
 
-campoB.addEventListener('keyup', function(event) {
-    console.log(event.target.value);
-    // formEValido = validaForm(event.target.value);
-    formEValido = validaForm(campoA.value,campoB.value)
-
-    if (!formEValido) {
-        campoB.classList.add('error')
-        document.querySelector('.error-message').style.display = 'block';
-    } else {
-        campoB.classList.remove('error')
-        document.querySelector('.error-message').style.display = 'none';
-    }
-})
+function recarregarPagina() {
+    location.reload();
+}
